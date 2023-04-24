86cb35a3a2560f7a4a6319c3b3998044f4709b35
Marco Goedert <marco_goedert@dell.com>
2023-04-23 22:03:42 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pucrs-sma-gerador-numeros-aleatorios
+
+Utilizando o conceito do método Congruente Linear para geração de números pseudo-aleatórios, programe um método que gere 1.000 (mil) números (entre os valores 0 e 1) a fim de produzir um gráfico de dispersão. 
+
+Você pode utilizar a linguagem de programação que desejar.
+
+A fim de validar seu método, envie um arquivo texto com os 1.000 números pseudo-aleatórios produzidos pelo seu método, o gráfico de dispersão destes números, e também um arquivo contendo os valores utilizados para as variáveis X0 (semente), a, c e M.
---
+++ main.py
@@ -0,0 +1,54 @@
+import argparse
+import time
+import matplotlib.pyplot as plt
+
+class CongruentLinearGenerator:
+    def __init__(self, seed=None, a=1664525, c=1013904223, m=2**32):
+        if seed is None:
+            self.seed = int(time.time())
+        else:
+            self.seed = seed
+        self.a = a
+        self.c = c
+        self.m = m
+    
+    def rand(self):
+        self.seed = (self.a * self.seed + self.c) % self.m
+        return self.seed / self.m
+
+def generate_numbers(clg, num):
+    numbers = []
+    for i in range(num):
+        numbers.append(clg.rand())
+    return numbers
+
+def save_numbers(numbers, filename):
+    with open(filename, 'w') as f:
+        for num in numbers:
+            f.write(str(num) + '\n')
+
+def save_variables(x0, a, c, m, filename):
+    with open(filename, 'w') as f:
+        f.write(f'x0={x0}\n')
+        f.write(f'a={a}\n')
+        f.write(f'c={c}\n')
+        f.write(f'm={m}\n')
+
+def plot_scatter(numbers):
+    plt.scatter(range(len(numbers)), numbers, s=1)
+    plt.xlabel('Índice')
+    plt.ylabel('Valor')
+    plt.title('Gráfico de Dispersão dos Números Pseudoaleatórios')
+    plt.savefig('scatter.png')
+    plt.show()
+
+# Argumentos de linha de comando
+parser = argparse.ArgumentParser(description='Gera números pseudoaleatórios usando o método congruente linear.')
+parser.add_argument('--seed', type=int, help='Define a semente do gerador.')
+args = parser.parse_args()
+
+clg = CongruentLinearGenerator(seed=args.seed)
+numbers = generate_numbers(clg, 1000)
+save_numbers(numbers, 'numbers.txt')
+save_variables(clg.seed, clg.a, clg.c, clg.m, 'variables.txt')
+plot_scatter(numbers)
